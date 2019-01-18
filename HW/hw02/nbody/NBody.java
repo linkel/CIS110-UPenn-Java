64b65dad486f2efe9cf08af493c0a01a1d5d789d
linkel <ironcarbide@gmail.com>
2019-01-17 21:49:26 -0800

    Completed NBody assignment...but results don't match exactly.

--- HW/hw02/nbody/NBody.java
+++ HW/hw02/nbody/NBody.java
@@ -34,10 +34,46 @@ public class NBody {
             img[i] = inStream.readString();
         }
 
+        double elapsedTime = 0.0;
+        PennDraw.enableAnimation(60);
+        double G = 6.67e-11; // grav constant        
+        while (elapsedTime <= simulationTime) {
+            PennDraw.setXscale(-radius, radius);
+            PennDraw.setYscale(-radius, radius);
+            PennDraw.picture(0.0,0.0,"starfield.jpg");
+            //PennDraw.picture(px[i],py[i],img[i]);
+            for (int i = 0; i < numParticles; i++) {
+                for (int j = 0; j < numParticles; j++) {
+                    if (j == i) {
+                        continue;
+                    }
+                    double deltaX = px[j] - px[i]; 
+                    double deltaY = py[j] - py[i];
+                    double d = Math.sqrt((deltaX*deltaX + deltaY*deltaY));
+                    //double force = ((G*m[i])/(d*d))*m[j];
+                    double force = (G*m[i] *m[j]) / (d * d);
+                    double forceX = force*(deltaX/d);
+                    double forceY = force*(deltaY/d);
+                    double accelX = forceX / m[i];
+                    double accelY = forceY / m[i];
+                    vx[i] = vx[i] + elapsedTime*accelX;
+                    vy[i] = vy[i] + elapsedTime*accelY;
+                }
+            }
+            elapsedTime += timeStep;
+            for (int i = 0; i < numParticles; i++) {
+                px[i] = px[i] + elapsedTime*vx[i];
+                py[i] = py[i] + elapsedTime*vy[i];
+                PennDraw.picture(px[i],py[i],img[i]);
+            }
+            PennDraw.advance();
+            
+        }
+
         System.out.printf("%d\n", numParticles);
         System.out.printf("%.2e\n", radius);
         for (int i = 0; i < numParticles; i++) {
             System.out.printf("%12.5e %12.5e %12.5e %12.5e %12.5e %12s\n", m[i], px[i], py[i], vx[i], vy[i], img[i]);
-}
+        }
     }
 }
