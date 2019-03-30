b9a7d3195fe1beaa5ec2ab0b1b82a20090c198ea
linkel <ironcarbide@gmail.com>
2019-03-30 13:55:44 -0700

    HW07 Tour Class implemented, except for draw point code.

--- HW/hw07/src/Tour.java
+++ HW/hw07/src/Tour.java
@@ -11,21 +11,70 @@
 //        void insertSmallest(Point p)    // insert p using the smallest increase heuristic
 
 public class Tour implements TourInterface {
+    public Node head;
+    public Node lastNode;
+
+    public Tour() {
+        this.head = null;
+        this.lastNode = null;
+    }
+
     public String toString() {
-        String bits = "hi";
-        return bits;
+        if (this.head == null) {
+            return "";
+        }
+        Node ptr = this.head;
+        String res = "";
+        while (ptr != null) {
+            res += ptr.point.toString();
+            res += "\n";
+            ptr = ptr.next;
+        }
+        return res;
     }
     public void draw(Point p) {
         PennDraw.clear();
     }
     public int size() {
-        return 3;
+        int count = 0;
+        Node ptr = this.head;
+        while (ptr != null) {
+            count += 1;
+            ptr = ptr.next;
+        }
+        return count;
     }
     public double distance() {
-        return 3.0;
+        double distance = 0;
+        Point firstPoint = null;
+        Point secondPoint = null;
+        Node ptr = this.head;
+        while (ptr != null) {
+            if (firstPoint == null) {
+                firstPoint = ptr.point;
+            } else if (secondPoint == null) {
+                secondPoint = ptr.point;
+                distance += firstPoint.distanceTo(secondPoint);
+            } else {
+                firstPoint = secondPoint;
+                secondPoint = ptr.point;
+                distance += firstPoint.distanceTo(secondPoint);
+            }
+            ptr = ptr.next;
+        }
+        return distance;
     }
     public void insertInOrder(Point p) {
-
+        if (this.head == null) {
+            this.lastNode = new Node(p);
+            this.head = new Node(this.lastNode, p);
+        } else {
+            Node ptr = this.head;
+            while (ptr != null && ptr.next != this.lastNode) {
+                ptr = ptr.next;
+            }
+            ptr.next = new Node(this.lastNode, p);
+        }
     }
     public void insertNearest(Point p) {
 
@@ -33,5 +82,19 @@ public class Tour implements TourInterface {
     public void insertSmallest(Point p) {
 
     }
+    public static void main(String[] args) {
+        Tour newTour = new Tour();
 
+        Point a = new Point(0, 0);
+        Point b = new Point(1, 0);
+        Point c = new Point(1, 1);
+        Point d = new Point(0, 1);
+        newTour.insertInOrder(a);
+        newTour.insertInOrder(b);
+        newTour.insertInOrder(c);
+        newTour.insertInOrder(d);
+        System.out.println(newTour.toString());
+        System.out.println(newTour.size());
+        System.out.println(newTour.distance());
+    }
 }
