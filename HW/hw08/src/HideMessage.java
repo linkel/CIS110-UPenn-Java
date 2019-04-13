eff95c09cae7efd907e5bc13a3abbea185585a87
linkel <ironcarbide@gmail.com>
2019-04-13 14:01:16 -0700

    Finished writing HideMessage, works for a test image.

--- HW/hw08/src/HideMessage.java
+++ HW/hw08/src/HideMessage.java
@@ -8,14 +8,23 @@ public class HideMessage {
         int[][] image = ImageData.load(args[0]);
         int image_size = image.length * image[0].length;
         image_size = image_size - (image_size % 7);
+
         In inFile = new In(args[1]);
+
         String message_to_hide = inFile.readAll();
         message_to_hide += "\0"; // null terminate the string
+
         int[] bit_message = Codec.encode(message_to_hide);
-        System.out.println(message_to_hide);
+
         if (message_to_hide.length() > image_size) {
             throw new RuntimeException("Message is bigger than image can hold");
         }
+        if (args.length == 4) {
+            String seed = args[2];
+            int tapPosition = Integer.parseInt(args[3]);
+            Codec.encrypt(bit_message, seed, tapPosition);
+        }
+
         int bit_ptr = 0;
         for (int i = 0; i < image.length ; i++) {
             for (int j = 0; j < image[0].length; j++) {
@@ -34,5 +43,6 @@ public class HideMessage {
             }
         }
         ImageData.show(image);
+        ImageData.save(image, "notencrypted.png");
     }
 }
